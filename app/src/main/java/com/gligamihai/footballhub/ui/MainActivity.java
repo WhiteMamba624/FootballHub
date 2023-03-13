0b7127522481afbed8a7e71416ecbb119883c0e6
Mihai Gliga <mihaigliga99@yahoo.com>
2023-03-13 20:29:12 +0200

    Users can now update events

--- app/src/main/java/com/gligamihai/footballhub/ui/MainActivity.java
+++ app/src/main/java/com/gligamihai/footballhub/ui/MainActivity.java
@@ -1,9 +1,14 @@
 package com.gligamihai.footballhub.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -13,16 +18,23 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.gligamihai.footballhub.R;
+import com.gligamihai.footballhub.Utils.EventAdapter;
 import com.gligamihai.footballhub.Utils.Main;
+import com.gligamihai.footballhub.models.Event;
 import com.gligamihai.footballhub.models.User;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
@@ -30,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     TextView currentUserEmail;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference documentRef = db.collection("Events");
+    private EventAdapter eventAdapter;
+    public static final String INTENT_EVENT_ID = "event_id";
+    public Event event = new Event();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,13 +59,96 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "User not logged in", Toast.LENGTH_SHORT).show();
         }
-        FloatingActionButton fab=findViewById(R.id.addEventFab);
+        setUpRecyclerView();
+        FloatingActionButton fab = findViewById(R.id.addEventFab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,AddEventActivity.class));
+                startActivity(new Intent(MainActivity.this, AddEventActivity.class));
             }
         });
+    }
+
+    public void setUpRecyclerView() {
+        Query query = documentRef.orderBy("eventDate", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Event> options = new FirestoreRecyclerOptions.Builder<Event>()
+                .setQuery(query, Event.class)
+                .build();
+        eventAdapter = new EventAdapter(options);
+        RecyclerView recyclerView = findViewById(R.id.events_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(eventAdapter);
+        removeSelectedEvent(recyclerView);
+    }
+
+    public void removeSelectedEvent(RecyclerView recyclerView) {
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull @NotNull RecyclerView recyclerView, @NonNull @NotNull RecyclerView.ViewHolder viewHolder, @NonNull @NotNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull @NotNull RecyclerView.ViewHolder viewHolder, int direction) {
+                String eventID = eventAdapter.getEventId(viewHolder.getBindingAdapterPosition());
+                getEventData(eventID);
+                if (FirebaseAuth.getInstance().getUid().equals(event.getOwnerId())) {
+                    new AlertDialog.Builder(viewHolder.itemView.getContext())
+                            .setMessage("Are you sure you want to delete this event?")
+                            .setPositiveButton("Yes", (dialog, which) -> eventAdapter.deleteEvent(viewHolder.getBindingAdapterPosition()))
+                            .setNegativeButton("No", (dialog, which) -> eventAdapter.notifyItemChanged(viewHolder.getBindingAdapterPosition()))
+                            .create()
+                            .show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Only the owner of the event can delete it", Toast.LENGTH_SHORT).show();
+                    eventAdapter.notifyItemChanged(viewHolder.getBindingAdapterPosition());
+                }
+            }
+        }).attachToRecyclerView(recyclerView);
+        eventAdapter.setOnItemClickListeners(new EventAdapter.OnItemClickListeners() {
+            @Override
+            public void onItemLongClick(DocumentSnapshot documentSnapshot, int position) {
+                String eventID=documentSnapshot.getId();
+                getEventData(eventID);
+                if (FirebaseAuth.getInstance().getUid().equals(event.getOwnerId())){
+                    Intent goToUpdateEventActivity=new Intent(MainActivity.this,UpdateEventActivity.class);
+                    goToUpdateEventActivity.putExtra(INTENT_EVENT_ID,eventID);
+                    startActivity(goToUpdateEventActivity);
+                } else {
+                    Toast.makeText(MainActivity.this, "Only the owner of this event can update it", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        eventAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        eventAdapter.stopListening();
+    }
+
+    public void getEventData(String docId) {
+        DocumentReference documentReference = db.collection("Events")
+                .document(docId);
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                event = documentSnapshot.toObject(Event.class);
+            }
+        });
+
     }
 
     public void clickMenu(View view) {
@@ -75,6 +175,7 @@ public class MainActivity extends AppCompatActivity {
     public void clickHome(View view) {
         recreate();
     }
+
     public void clickLogout(View view) {
         AlertDialog.Builder alertLogout = new AlertDialog.Builder(view.getContext());
         alertLogout.setTitle("Logout");
@@ -92,19 +193,19 @@ public class MainActivity extends AppCompatActivity {
         alertLogout.show();
     }
 
-    public void clickProfile(View view){
-        startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+    public void clickProfile(View view) {
+        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
     }
 
-    public void clickWeather(View view){
-        startActivity(new Intent(MainActivity.this,WeatherActivity.class));
+    public void clickWeather(View view) {
+        startActivity(new Intent(MainActivity.this, WeatherActivity.class));
     }
 
-    public void clickPlaces(View view){
-        startActivity(new Intent(MainActivity.this,PlacesActivity.class));
+    public void clickPlaces(View view) {
+        startActivity(new Intent(MainActivity.this, PlacesActivity.class));
     }
 
-    public void clickCaloriesCalculator(View view){
-        startActivity(new Intent(MainActivity.this,CaloriesCalculatorActivity.class));
+    public void clickCaloriesCalculator(View view) {
+        startActivity(new Intent(MainActivity.this, CaloriesCalculatorActivity.class));
     }
 }
---
+++ app/src/main/java/com/gligamihai/footballhub/ui/UpdateEventActivity.java
@@ -0,0 +1,162 @@
+package com.gligamihai.footballhub.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.gligamihai.footballhub.R;
+import com.gligamihai.footballhub.models.Event;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Calendar;
+
+import static com.gligamihai.footballhub.ui.MainActivity.INTENT_EVENT_ID;
+
+public class UpdateEventActivity extends AppCompatActivity {
+    final Calendar calendar = Calendar.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public String id=null;
+    EditText updateRecommendedPlayerExperienceLevel;
+    EditText updateMatchDayTitle;
+    EditText updateEventDate;
+    EditText updateEventStartTime;
+    EditText updateEventLength;
+    EditText updateEventLocation;
+    EditText updateNumberOfTeams;
+    EditText updateNumberOfPlayersPerTeam;
+    EditText updateEventCost;
+    Button updateEventButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_event);
+        if (getIntent() != null) {
+            id = getIntent().getStringExtra(INTENT_EVENT_ID);
+        }
+        updateRecommendedPlayerExperienceLevel=findViewById(R.id.editTextUpdateRecommendedPlayerLevel);
+        updateMatchDayTitle=findViewById(R.id.editTextUpdateMatchDayTitle);
+        updateEventDate=findViewById(R.id.editTextUpdateEventDate);
+        updateEventStartTime=findViewById(R.id.editTextUpdateEventTime);
+        updateEventLength=findViewById(R.id.editTextUpdateEventLength);
+        updateEventLocation=findViewById(R.id.editTextUpdateEventLocation);
+        updateNumberOfTeams=findViewById(R.id.editTextUpdateEventNumberOfTeams);
+        updateNumberOfPlayersPerTeam=findViewById(R.id.editTextUpdateEventNumberOfPlayersPerTeam);
+        updateEventCost=findViewById(R.id.editTextUpdateEventCost);
+        getEvent(id);
+        updateEventButton=findViewById(R.id.updateEventButton);
+        updateEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Event event=new Event();
+                event.setOwnerId(FirebaseAuth.getInstance().getUid());
+                event.setRecommendedPlayerExperienceLevel(updateRecommendedPlayerExperienceLevel.getText().toString().trim());
+                event.setMatchDayTitle(updateMatchDayTitle.getText().toString().trim());
+                event.setEventDate(updateEventDate.getText().toString().trim());
+                event.setEventStartTime(updateEventStartTime.getText().toString().trim());
+                event.setEventLength(Integer.parseInt(updateEventLength.getText().toString().trim()));
+                event.setEventLocation(updateEventLocation.getText().toString().trim());
+                event.setNumberOfTeams(Integer.parseInt(updateNumberOfTeams.getText().toString().trim()));
+                event.setNumberOfPlayersPerTeam(Integer.parseInt(updateNumberOfPlayersPerTeam.getText().toString().trim()));
+                event.setEventCost(Integer.parseInt(updateEventCost.getText().toString().trim()));
+                updateEvent(event);
+            }
+        });
+    }
+
+    public void goBackToMainActivity(View view) {
+        onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(UpdateEventActivity.this,MainActivity.class));
+    }
+
+    public void datePicker(View view) {
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        updateEventDate.setText(String.format("%d-%d-%d", dayOfMonth, monthOfYear + 1, year));
+
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
+    }
+
+    public void timePicker(View view) {
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+                        updateEventStartTime.setText(String.format("%d:%d", hourOfDay, minute));
+                    }
+                }, hour, minute, true);
+        timePickerDialog.show();
+    }
+
+    public void getEvent(String id){
+        DocumentReference documentReference=db.collection("Events")
+                .document(id);
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Event event=documentSnapshot.toObject(Event.class);
+                updateRecommendedPlayerExperienceLevel.setText(event.getRecommendedPlayerExperienceLevel());
+                updateMatchDayTitle.setText(event.getMatchDayTitle());
+                updateEventDate.setText(event.getEventDate());
+                updateEventStartTime.setText(event.getEventStartTime());
+                updateEventLength.setText(String.valueOf(event.getEventLength()));
+                updateEventLocation.setText(event.getEventLocation());
+                updateNumberOfTeams.setText(String.valueOf(event.getNumberOfTeams()));
+                updateNumberOfPlayersPerTeam.setText(String.valueOf(event.getNumberOfPlayersPerTeam()));
+                updateEventCost.setText(String.valueOf(event.getEventCost()));
+            }
+        });
+    }
+
+    public void updateEvent(Event event){
+        db.collection("Events")
+                .document(id)
+                .set(event)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(UpdateEventActivity.this, "Event updated successfully", Toast.LENGTH_SHORT).show();
+                            onBackPressed();
+                        }else{
+                            Toast.makeText(UpdateEventActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
